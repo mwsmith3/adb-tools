ff67fdd571bda0bd1491f130874362f839179ed9
Matthew Smith <mwsmith3@googlemail.com>
2020-11-14 18:57:44 +0000

    get those deep links working

--- src/main/kotlin/com/github/mwsmith3/adbtools/util/DeepLinkParser.kt
+++ src/main/kotlin/com/github/mwsmith3/adbtools/util/DeepLinkParser.kt
@@ -1,5 +1,7 @@
 package com.github.mwsmith3.adbtools.util
 
+import android.net.Uri
+import com.android.ide.common.xml.AndroidManifestParser
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel
 import com.android.tools.idea.util.androidFacet
 import com.intellij.openapi.project.Project
@@ -32,26 +34,74 @@ object DeepLinkParser {
                 Pair(pair.first, it)
             }
         }
-        return dataElements.mapNotNull { getDeepLinkFromData(it.first, it.second) }
+        return dataElements.flatMap { getDeepLinkFromData(it.first, it.second) }
     }
 
-    private fun getDeepLinkFromData(facet: AndroidFacet, data: Data): DeepLink? {
-        val attributes = data.xmlTag?.attributes?.toList().orEmpty()
-        val scheme = attributes.find { it.name == "android:scheme" }?.value?.resolveResource(facet)
-        val host = attributes.find { it.name == "android:host" }?.value?.resolveResource(facet)
-        val pathPrefix = attributes.find { it.name == "android:pathPrefix" }?.value?.resolveResource(facet)
+    private fun getDeepLinkFromData(facet: AndroidFacet, data: Data): List<DeepLink> {
+        // TODO it should work according to this https://developer.android.com/guide/topics/manifest/data-element
+
+
         val packageName = facet.let {
             AndroidModuleModel.get(it)?.applicationId
+        } ?: return emptyList()
+
+        val attributes = data.xmlTag?.attributes?.toList().orEmpty()
+        val schemes = attributes.filter {
+            it.name == "android:scheme"
+        }.mapNotNull {
+            it.value?.resolveResource(facet)
         }
-        return if (packageName != null && scheme != null && host != null) {
-            DeepLink(packageName, scheme, host, pathPrefix)
+
+        val hosts = attributes.filter {
+            it.name == "android:host"
+        }.mapNotNull {
+            it.value?.resolveResource(facet)
+        }
+
+        val paths = if (attributes.find { it.name == "android.path" } == null) {
+            attributes.filter {
+                it.name == "android:pathPrefix"
+            }
         } else {
-            null
+            attributes.filter {
+                it.name == "android:path"
+            }
+        }.mapNotNull {
+            it.value?.resolveResource(facet)
         }
+
+        val links = mutableListOf<DeepLink>()
+        schemes.forEach { scheme ->
+            hosts.forEach { host ->
+                paths.forEach { path ->
+                    links += DeepLink(packageName, scheme, host, path)
+                }
+            }
+        }
+        return links
+
+//        val scheme = attributes.find { it.name == "android:scheme" }?.value?.resolveResource(facet)
+//
+//
+//        val host = attributes.find { it.name == "android:host" }?.value?.resolveResource(facet)
+//
+//
+//        val pathPrefix = attributes.find { it.name == "android:pathPrefix" }?.value?.resolveResource(facet)
+//
+//
+////        val packageName = facet.let {
+////            AndroidModuleModel.get(it)?.applicationId
+////        }
+//        return if (packageName != null && scheme != null && host != null) {
+//            DeepLink(packageName, scheme, host, pathPrefix)
+//        } else {
+//            null
+//        }
     }
 }
 
 fun String.resolveResource(facet: AndroidFacet): String {
+    // TODO use AndroidDomUtil.getResourceReferenceConverter()
     return if (this.startsWith("@string/")) {
         val key = this.removePrefix("@string/")
         GetAndroidStrings.getValue(facet, key) ?: ""
@@ -60,8 +110,8 @@ fun String.resolveResource(facet: AndroidFacet): String {
     }
 }
 
-data class DeepLink(val packageName: String, val scheme: String, val host: String, val pathPrefix: String?) {
-    val link = "${scheme}://${host}${pathPrefix ?: ""}"
+data class DeepLink(val packageName: String, val scheme: String, val host: String, val path: String?) {
+    val link = "${scheme}://${host}${path ?: ""}"
     override fun toString(): String {
         return link
     }
