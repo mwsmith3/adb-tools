7aab0c051a667a602df4e042e5359f51c61db744
Matthew Smith <mwsmith3@googlemail.com>
2021-07-06 14:52:24 +0100

    updated instantiating NotificationGroups due to constructor deprecation

--- src/main/kotlin/com/github/mwsmith3/adbtools/util/Notifications.kt
+++ src/main/kotlin/com/github/mwsmith3/adbtools/util/Notifications.kt
@@ -1,16 +1,14 @@
 package com.github.mwsmith3.adbtools.util
 
-import com.intellij.notification.NotificationDisplayType
 import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 
 object Notifications {
     private const val NOTIFICATION_TITLE = "ADB Tools"
-    val INFO_GROUP = NotificationGroup("ADB Logs", NotificationDisplayType.NONE, true, null, null)
-    val ERROR_GROUP = NotificationGroup("ADB Error", NotificationDisplayType.BALLOON, true, null, null)
-    val STICKY_GROUP =
-        NotificationGroup("ADB Warning", NotificationDisplayType.STICKY_BALLOON, true, null, null)
+    private val INFO_GROUP = NotificationGroup.logOnlyGroup("ADB Logs")
+    private val ERROR_GROUP = NotificationGroup.balloonGroup("ADB Error")
+    val WARNING_GROUP = NotificationGroup.balloonGroup("ADB Warning")
 
     fun info(message: String, project: Project? = null) {
         return INFO_GROUP.createNotification(
